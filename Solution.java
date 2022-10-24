eb57a3cd594c8b7035cd4dc5d414eb192d4b4023
Samuel Gachanja Kangori <gksamuel1@gmail.com>
2022-10-24 06:30:00 +0300

    A very big sum - sum of entries in a List

--- Solution.java
+++ Solution.java
@@ -1,41 +1,16 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Scanner;
 class Solution{
-
 	public static void main(String[] args){
-		List<Integer> alice = new ArrayList<Integer>();
-		List<Integer> bob = new ArrayList<Integer>();
-		alice.add(1);
-		alice.add(5);
-		alice.add(8);
-		bob.add(3);
-		bob.add(6);
-		bob.add(1);
-		List<Integer> res = compareTriplets(alice, bob);
-		for(Integer i: res){
-			System.out.print(i + " ");
+		Scanner scanner = new Scanner(System.in);
+		int items = scanner.nextInt();
+		scanner.nextLine();
+		String entries = scanner.nextLine();
+		scanner.close();
+		String[] nums = entries.split(" ");
+		Long result = 0L;
+		for(String s: nums){
+			result = result + Long.valueOf(s);
 		}
-		System.out.println("");
-
-
-	}
-
-	public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b){
-		int ascore = 0;
-		int bscore = 0;
-		for(int x = 0; x < a.size(); x++){
-			if(a.get(x) > b.get(x)){
-				ascore++;
-			}
-			if(b.get(x) > a.get(x)){
-				bscore++;
-			}
-		}
-		List<Integer> result = new ArrayList<Integer>();
-		result.add(ascore);
-		result.add(bscore);
-
-		return result;
-
+		System.out.println(result);
 	}
 }
