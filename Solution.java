e2cfebbae2825bd68e9ca11212d68006e2b0e7b9
Samuel Gachanja Kangori <gksamuel1@gmail.com>
2022-10-25 05:48:28 +0300

    Check if a regular expression is valid

--- Solution.java
+++ Solution.java
@@ -1,16 +1,36 @@
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import java.util.regex.Matcher;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+
 class Solution{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		int items = scanner.nextInt();
+		int entries = scanner.nextInt();
 		scanner.nextLine();
-		String entries = scanner.nextLine();
-		scanner.close();
-		String[] nums = entries.split(" ");
-		Long result = 0L;
-		for(String s: nums){
-			result = result + Long.valueOf(s);
+		List<String> patterns = new ArrayList<String>();
+		for(int x = 0; x < entries; x++){
+			patterns.add(scanner.nextLine());
 		}
-		System.out.println(result);
+		scanner.close();
+		for(String s : patterns){
+			try{
+				Pattern pattern = Pattern.compile(s);
+				System.out.println("Valid");
+
+			}
+			catch(PatternSyntaxException pse){
+				System.out.println("Invalid");
+
+			}
+
+
+		}
+
+
+
 	}
 }
