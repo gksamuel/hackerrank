e459310038e0367153cdadf10306b7b5d18b664f
Samuel Gachanja Kangori <gksamuel1@gmail.com>
2022-10-26 05:57:19 +0300

    Absolute Diagonal difference of an array

--- Solution.java
+++ Solution.java
@@ -1,36 +1,41 @@
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-import java.util.regex.Matcher;
-import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-
-
 class Solution{
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		int entries = scanner.nextInt();
-		scanner.nextLine();
-		List<String> patterns = new ArrayList<String>();
-		for(int x = 0; x < entries; x++){
-			patterns.add(scanner.nextLine());
+		List<Integer> a = new ArrayList<Integer>();
+		a.add(1);
+		a.add(2);
+		a.add(3);
+		List<Integer> b = new ArrayList<Integer>();
+		b.add(4);
+		b.add(5);
+		b.add(6);
+		List<Integer> c = new ArrayList<Integer>();
+		c.add(9);
+		c.add(8);
+		c.add(9);
+
+		List<List<Integer>> input = new ArrayList<List<Integer>>();
+		input.add(a);
+		input.add(b);
+		input.add(c);
+		int result = diagonalDifference(input);
+		System.out.println(result);
+
+
+	}
+
+	public static int diagonalDifference(List<List<Integer>> arr){
+		int ltrSum = 0;
+		int rtlSum = 0;
+		int ltrPos = 0;
+		int rtlPos = arr.size() - 1;
+		for(List<Integer> row: arr){
+			ltrSum = ltrSum + row.get(ltrPos);
+			rtlSum = rtlSum + row.get(rtlPos);
+			ltrPos++;
+			rtlPos--;
 		}
-		scanner.close();
-		for(String s : patterns){
-			try{
-				Pattern pattern = Pattern.compile(s);
-				System.out.println("Valid");
-
-			}
-			catch(PatternSyntaxException pse){
-				System.out.println("Invalid");
-
-			}
-
-
-		}
-
-
-
+		return Math.abs(ltrSum - rtlSum);
 	}
 }
